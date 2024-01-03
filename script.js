2b7c090713991f24bb013daf203aedb9a1190b0c
Darine <tagdarinefatma@gmail.com>
2024-01-03 23:10:22 +0100

    add a variable

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+let something = 2024;
 
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
@@ -17,3 +18,18 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+}
+
+function goCave() {
+    button2.innerText = "Buy weapon (30 gold)";
+  }
+  function fightDragon() {
+    button3.innerText = "Go to town square";
+  }
