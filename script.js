fe975bd967e84c52857df2593d7977c65925f2d5
Darine <tagdarinefatma@gmail.com>
2024-01-05 20:49:32 +0100

    add function to buy weapons

--- script.js
+++ script.js
@@ -77,6 +77,19 @@ function buyHealth() {
   goldText.innerText = gold;
 healthText.innerText = health;
 }
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a " + newWeapon + ".";
+    inventory.push(newWeapon);
+    text.innerText += " In your inventory you have: " + inventory;
+  } else {
+    text.innerText = "You do not have enough gold to buy a weapon.";
+  }
+}
 
 function buyWeapon() {}
 
