b35541595f5b4ec3dadeb0d8877deda28d7b5cea
Darine <tagdarinefatma@gmail.com>
2024-01-10 12:31:46 +0100

    add defeatMonster function

--- script.js
+++ script.js
@@ -169,8 +169,24 @@ function attack() {
   health -= monsters[fighting].level;
   monsterHealth -=
     weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
-    healthText.innerText = health;
+  healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
+  if (health <= 0) {
+    lose();
+  } else if (monsterHealth <= 0) {
+    defeatMonster();
+  }
 }
 
-function dodge() {}
+function dodge() {
+  text.innerText = "You dodge the attack from the " + monsters[fighting].name;
+}
+
+function defeatMonster() {
+  gold += Math.floor(monsters[fighting].level * 6.7);
+  xp += monsters[fighting].level;
+  goldText.innerText = gold;
+  xpText.innerText = xp;
+  update(locations[4]);
+}
+function lose() {}
