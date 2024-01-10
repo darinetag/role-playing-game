7fd7b2e3c8d893a79f3b572b87f240933cf9773b
Darine <tagdarinefatma@gmail.com>
2024-01-10 12:16:25 +0100

    add attack function

--- script.js
+++ script.js
@@ -158,9 +158,19 @@ function goFight() {
   update(locations[3]);
   monsterHealth = monsters[fighting].health;
   monsterStats.style.display = "block";
-
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsters[fighting].health;
 }
 
-function attack() {}
+function attack() {
+  text.innerText = "The " + monsters[fighting].name + " attacks.";
+  text.innerText +=
+    " You attack it with your " + weapons[currentWeapon].name + ".";
+  health -= monsters[fighting].level;
+  monsterHealth -=
+    weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
+}
 
 function dodge() {}
