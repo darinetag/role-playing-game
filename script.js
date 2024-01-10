416c8a591a834ce3b16b615f1864e2eab3c477eb
Darine <tagdarinefatma@gmail.com>
2024-01-10 11:06:08 +0100

    update functions

--- script.js
+++ script.js
@@ -156,6 +156,9 @@ function fightDragon() {
 }
 function goFight() {
   update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+
 }
 
 function attack() {}
